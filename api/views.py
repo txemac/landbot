b199e89b86228b0109aa12ee0f6fb47840c37da1
txemac <txema.bermudez@gmail.com>
2025-02-04 13:02:55 +0100

    Add new app drf_yasg New serializer to health response Add extra documentation at endpoint

--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
 from datetime import UTC
 from datetime import datetime
 
+from api.serializer import HealthSerializer
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from landbot.settings import API_TITLE
 from landbot.settings import API_VERSION
 from rest_framework import status
@@ -9,14 +12,23 @@ from rest_framework.views import APIView
 
 
 class HealthView(APIView):
+
+    @swagger_auto_schema(
+        tags=["Health"],
+        operation_summary="Health check",
+        operation_description="Checks the health status of the system.",
+        responses={status.HTTP_200_OK: openapi.Response(description="Health status OK", schema=HealthSerializer)},
+    )
     def get(
         self,
         request,
     ) -> Response:
-        response_data = dict(
-            title=API_TITLE,
-            status="OK",
-            version=API_VERSION,
-            time=datetime.now(UTC),
+        return Response(
+            dict(
+                title=API_TITLE,
+                status="OK",
+                version=API_VERSION,
+                time=datetime.now(UTC),
+            ),
+            status=status.HTTP_200_OK,
         )
-        return Response(response_data, status=status.HTTP_200_OK)
