152044f424f92c87bebf87156f840cc5a7dcad75
txemac <txema.bermudez@gmail.com>
2025-02-07 19:57:03 +0100

    Improve CI

--- landbot/settings.py
+++ landbot/settings.py
@@ -107,3 +107,13 @@ CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
 CELERY_TIMEZONE = "UTC"
+
+# Email
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "email@gmail.com"
+EMAIL_HOST_PASSWORD = "email_password"
+DEFAULT_FROM_EMAIL = "email@gmail.com"
+RECIPIENT_LIST = ["email1@gmail.com", "email2@gmail.com"]
